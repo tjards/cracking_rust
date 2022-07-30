b90f69ee06e51b51819d675b45abd6293be0a504
tjards <travis.jardine@gmail.com>
2022-07-30 15:34:58 -0400

    started question 1-1

---
+++ src/ch01_q01_is_unique.rs
@@ -0,0 +1,13 @@
+/*
+
+Title: Is Unique 
+
+Description: Implement an algo to determine if a string 
+has all unique characters. What if you cannot use
+additional data structures?
+
+*/
+fn main(){
+    println!("Chapter 01, Question 01 running...")
+}
+
